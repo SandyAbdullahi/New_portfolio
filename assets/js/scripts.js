76cec49d35206233a460154cc8fa45ee5f89d03d
SandyAbdullahi <ab2d222@gmail.com>
2025-07-29 17:18:43 +0300

    add new sections

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,16 @@
   const toggleBtn  = document.getElementById('mobileMenuToggle');
   const mobileMenu = document.getElementById('mobileMenu');
 
-  toggleBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-    // circle fill effect
-    toggleBtn.classList.toggle('bg-lime-200');
-    toggleBtn.classList.toggle('border-lime-200');
-    toggleBtn.classList.toggle('text-[#1A2318]');
-  });
+//   toggleBtn.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('hidden');
+//     // circle fill effect
+//     toggleBtn.classList.toggle('bg-lime-200');
+//     toggleBtn.classList.toggle('border-lime-200');
+//     toggleBtn.classList.toggle('text-[#1A2318]');
+//   });
+toggleBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  document.body.classList.toggle('nav-open');   // toggle the X state
+  toggleBtn.classList.toggle('bg-lime-200');    // optional circle fill
+});
+
